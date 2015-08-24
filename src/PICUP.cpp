6679c13e2cfc971c1e70df36786d2dddd675eaa4
bcornille <bcornille7@gmail.com>
2015-08-24 15:39:06 -0500

    Set up more Doxygen options and added Doxygen style comments to PICUP.ccpp.

--- src/PICUP.cpp
+++ src/PICUP.cpp
@@ -1,8 +1,9 @@
 /***************************************************************************//*!
  * \file
  * \brief This is the main program file for the Particle-In-Cell Utility for
- * Practice (PICUP).
+ * 			Practice (PICUP).
  *
+ * Longer description will go here.
  ******************************************************************************/
 
 int main(int argc, char *argv[])
