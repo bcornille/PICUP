2f0bb3ad7a93f3a7b39cb842f04faefeb91651ec
bcornille <bcornille7@gmail.com>
2015-08-24 13:26:27 -0500

    Started the PICUP framework.

---
+++ src/PICUP.cpp
@@ -0,0 +1,12 @@
+/***************************************************************************//*!
+ * \file
+ * \brief This is the main program file for the Particle-In-Cell Utility for
+ * Practice (PICUP).
+ *
+ ******************************************************************************/
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
