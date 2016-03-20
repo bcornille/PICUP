33fa8097b49d9f7fad0b44273a442510849c8faa
bcornille <bcornille7@gmail.com>
2016-03-20 00:38:03 -0500

    Working out finite element stuff.

--- src/boundary.hpp
+++ src/boundary.hpp
@@ -7,4 +7,23 @@
  *
  */
 
+#include "mesh.hpp"
 
+#ifndef _boundary_hpp
+#define _boundary_hpp
+
+//! A template class for the boundary base class.
+/*!
+ * The boundary class should point to sections of the elements on the
+ * boundary. Elements should probably be abstracted better in the code
+ * before developing the boundary class.
+ */
+template <typename PosInd, typename PosVec>
+class Boundary
+{
+	protected:
+
+	public:
+}
+
+#endif //_boundary_hpp
---
+++ src/elements.hpp
@@ -0,0 +1,24 @@
+/*!
+ * \file
+ * \brief Elements header file.
+ *
+ * Contents:
+ *
+ */
+
+#ifndef _elements_hpp
+#define _elements_hpp
+
+template <typename PosVec>
+stuct Node
+{
+	PosVec coordinates;
+};
+
+template <typename PosVec, int poly_deg>
+class Element
+{
+	private:
+};
+
+#endif //_elements_hpp
