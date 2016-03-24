252c4eb7abec9e56b4693902c7623c7e5fd26fa0
bcornille <bcornille7@gmail.com>
2016-03-24 16:08:34 -0500

    Starting to figure out how finite elements should be implemented. This will require a substantial rewrite of the other class templates.

--- src/elements.hpp
+++ src/elements.hpp
@@ -9,16 +9,25 @@
 #ifndef _elements_hpp
 #define _elements_hpp
 
-template <typename PosVec>
-stuct Node
+template <int pdim>
+class Node
 {
-	PosVec local_coords;
+	protected:
+		Eigen::Matrix<double, pdim, 1> global_coords;
+
+		int global_id
 };
 
-template <typename PosVec, int poly_deg>
+template <int pdim>
 class Element
 {
 	protected:
+};
+
+template <int pdim>
+class Vertex : public Node
+{
+	private:
 };
 
 #endif //_elements_hpp
